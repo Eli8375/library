e35de1a1ce223bd23664eff0b1bd4fa986871d3f
IJA <alekajobs@gmail.com>
2022-08-15 14:38:15 -0700

    implemented user input book objects and will display to the user successively

--- script.js
+++ script.js
@@ -1,80 +1,71 @@
-let title = document.getElementById('title');
-let author = document.getElementById('author');
-let pages = document.getElementById('pages');
-let status = document.getElementById('status');
-
-
-let tbody = document.querySelector('.cells');
-let newTD = document.createElement('td');
-let newTR = document.createElement('tr');
-
-//let newRow = tbody.append(newTR);
-
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
-    this.info = info = () => {
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.status} yet`);
-    }
 }
 
-
+function createDeleteButton() {
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerText = "delete";
+    return deleteButton;
+}
 
 function addBookToLibrary(myLibrary, title, author, pages, status) {
-    // if (title.value === '' || author.value === '') {
-    //     return;
-    // }
-    // if (pages.value === typeof("")) {
-    //     alert("Please enter a number for the pages field.");
-    //     return;
-    // }
     let newBook = new Book(title, author, pages, status);
     myLibrary.push(newBook);
     console.log(myLibrary);
 }
 
 
-function displayEachBook(myLibrary) {
-    for (let i = 0; i < myLibrary.length; i++) {
-        tbody.append(newTR);
-        for (let j = 0; j < myLibrary.length; j++) {
-            
-            tbody.append(newTD)//.append(myLibrary[j].title);
-            tbody.append(newTD)//.append(myLibrary[j].author);
-            tbody.append(newTD)//.append(myLibrary[j].pages);
-            
-            // let newCell4 = newRow.append(newTD);
-            // newCell4.innerText = myLibrary[j].pages;
-            // let newCell5 = newRow.append(newTD);
-            // newCell5.innerText = myLibrary[j].pages;
-        }
+function displayEachBook(myLibrary, tableID) {
+    let tableRef = document.getElementById(tableID);
+    for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+        let newRow = tableRef.insertRow(0);
+        let newCell = newRow.insertCell(0);
+        newCell.append(myLibrary[i].title);
+        let newCell2 = newRow.insertCell(1);
+        newCell2.append(myLibrary[i].author);
+        let newCell3 = newRow.insertCell(2);
+        newCell3.append(myLibrary[i].pages);
+        let newCell4 = newRow.insertCell(3);
+        newCell4.append(myLibrary[i].status);
+        deleteButton = createDeleteButton();
+        let newCell5 = newRow.insertCell(4);
+        newCell5.append(deleteButton);
     }
-    
 }
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read');
-// myLibrary.push(theHobbit);
-
-
 function main() {
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let pages = document.getElementById('pages');
+    let status = document.getElementById('status');
+    let submitButton = document.getElementById('submit-button');
+
+    console.log(title.value)
 
     let myLibrary = [];
 
-
-
-
-    
-    addBookToLibrary(myLibrary, 'One more thing', 'some text', '522', 'read');
-    addBookToLibrary(myLibrary, 'Something', 'different text', '321', 'read');
-    addBookToLibrary(myLibrary, 'Another thing', 'some other text', '765', 'read');
-    addBookToLibrary(myLibrary, 'One more thing', 'some text', '522', 'read');
-    addBookToLibrary(myLibrary, 'Something', 'different text', '321', 'read');
-
-    displayEachBook(myLibrary);
+    submitButton.addEventListener("click", () => {
+        if (title.value !== "" && author.value !== "" && pages.value !== "") {
+            addBookToLibrary(myLibrary, title.value, author.value, pages.value, status.value);
+            displayEachBook(myLibrary, 'cells')
+        }
+    })
 
     
+
 }
 
-main()
+main();
+
+/*
+remove algo
+user will click delete button
+this will search the array for the location where the user clicked
+it will utilize pop to remove the array element
+it will refresh the page to display the current array to the user
+that specific array element must have been removed and the other elements moved down in lieu
+*/
